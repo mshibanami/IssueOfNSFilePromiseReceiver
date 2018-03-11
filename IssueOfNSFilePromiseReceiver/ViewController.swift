cc440e1ba8d4628b8c0387f14b70a87da6fefd3c
Manabu Nakazawa <mshibanami@gmail.com>
2018-03-11 13:49:24 +0900

    init

---
+++ IssueOfNSFilePromiseReceiver/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  IssueOfNSFilePromiseReceiver
+//
+//  Created by abc on 2018/03/11.
+//  Copyright © 2018 mshibanami. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
